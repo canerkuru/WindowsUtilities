4e748f83b15c2f6ec51250250a56d8bd8b907661
Caner Kuru <caner.kuru@yahoo.com>
2023-08-09 18:18:03 +0300

    RSA

---
+++ WindowsRSAKeyContainerList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+var files = Directory.GetFiles(@"C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys\");
+var errors = new List<string>();
+
+foreach (var f in files)
+{
+    try
+    {
+        var bytes = File.ReadAllBytes(f);
+        var containerName = Encoding.ASCII.GetString(bytes, 40, bytes[8] - 1);
+
+        Console.WriteLine("containerName: " + containerName + " fileName: "+ f);
+    }
+    catch (Exception e)
+    {
+        errors.Add(e.Message);
+    }
+}
+
+if (errors.Any())
+{
+    Console.WriteLine("\n\nErrors:");
+
+    foreach (var e in errors)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+Console.Read();
